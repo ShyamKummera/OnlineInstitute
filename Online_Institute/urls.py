00243e2a188d58a9c738a60501101956a9667657
shyam kummera <63773053+ShyamKummera@users.noreply.github.com>
2020-07-13 22:24:12 +0530

    Student main page nav bar completed

--- Online_Institute/urls.py
+++ Online_Institute/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 from app1 import views
+from student import urls as st
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include(st)),
 # --------------------------------------------------
     path('',views.mainpage,name="mainpage"),
 # -----------------admin main button--------------------------
